5c71f90eb4c9e4a13b25199402d5d722d210331a
sandevh <164613364+sandevh@users.noreply.github.com>
2025-01-31 09:18:40 +0530

    Feature/splash screen (#1)
    * implemented basic splash screen
    
    * implemented basic splash screen

---
+++ kuppi-interface/screens/SplashScreen.js
@@ -0,0 +1,28 @@
+import { StyleSheet, Image, View } from "react-native";
+
+SplashScreen = () => {
+
+  return (
+    <View style={styles.container}>
+      <View>
+        {/*sample logo*/}
+        <Image source={require("../assets/kuppi-icon.png")} style={styles.image} />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  image: {
+    height: 150,
+    width: 150
+  }
+});
+
+export default SplashScreen;
