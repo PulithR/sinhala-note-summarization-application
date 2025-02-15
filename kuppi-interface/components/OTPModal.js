b6919aafdbc0b7e83aeb3b2406b808ca455d5f7d
sandevh <sandevhettiarachchi@gmail.com>
2025-02-16 01:19:07 +0530

    started with a basic frontend modal to get OTP input

---
+++ kuppi-interface/components/OTPModal.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Modal,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
+
+const OTPModal = () => {
+  const [otp, setOtp] = useState("");
+
+  const handleVerifyOtp = () => {};
+
+  return (
+    <Modal animationType="slide">
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={styles.modalBackground}>
+          <View style={styles.modalContainer}>
+            <Text>Enter OTP</Text>
+            <Text>We have sent a verification code to your email.</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Enter OTP"
+              value={otp}
+              onChangeText={setOtp}
+              keyboardType="numeric"
+            />
+
+            <TouchableOpacity onPress={handleVerifyOtp}>
+              <Text>Verify OTP</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <Text>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </TouchableWithoutFeedback>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modalBackground: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+  modalContainer: {
+    backgroundColor: "#fff",
+    padding: 20,
+    borderRadius: 8,
+    alignItems: "center",
+  },
+  input: {
+    width: "100%",
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    marginVertical: 10,
+    paddingHorizontal: 10,
+  },
+});
+
+export default OTPModal;
