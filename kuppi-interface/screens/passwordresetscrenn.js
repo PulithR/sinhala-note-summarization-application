0abc11182a6f734c241903e3a8ae6e7efdd19e73
Reshan SIlva <saveeshasilva2003@gmail.com>
2025-02-16 21:08:01 +0530

    Add chat UI with animations and keyboard handling

---
+++ kuppi-interface/screens/passwordresetscrenn.js
@@ -0,0 +1,16 @@
+import React, { useState, useRef, useEffect } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Animated,
+  ScrollView,
+  KeyboardAvoidingView,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Platform,
+  Image,
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
