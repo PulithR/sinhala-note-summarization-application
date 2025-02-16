6b18912e1dc8fce75711bcd09612995bc4a5cfec
Sandaru04 <sadmal04that@gmail.com>
2025-02-16 20:26:12 +0530

    Added base component structure

--- kuppi-interface/screens/SummarizerScreen.js
+++ kuppi-interface/screens/SummarizerScreen.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import { View, Text, SafeAreaView } from "react-native";
 
 const SummarizerScreen = () => {
-
-}
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Summarizer</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
 
 export default SummarizerScreen;
