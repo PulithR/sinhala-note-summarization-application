a0daa1b148639b7051da999aa721b9c927c7e3a9
sandevh <sandevhettiarachchi@gmail.com>
2025-02-20 11:39:35 +0530

    added note service and implemented add note service

---
+++ kuppi-server/services/notes_service.py
@@ -0,0 +1,21 @@
+from flask_jwt_extended import get_jwt_identity
+from models.user_model import users_db
+
+def add_note_service(data):
+    current_user_email = get_jwt_identity()
+    user = users_db.get(current_user_email)
+
+    if not user:
+        return {"error": "User not found"}, 404
+
+    title = data.get("title")
+    content = data.get("content")
+
+    if not title or not content:
+        return {"error": "Title and content are required!"}, 400
+
+    note_id = len(user["notes"]) + 1
+    note = {"id": note_id, "title": title, "content": content}
+    user["notes"].append(note)
+
+    return {"success": True, "note": note}, 201
