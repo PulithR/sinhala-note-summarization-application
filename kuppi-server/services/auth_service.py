1a8c7e8eb3a61a6a206e0a76442cc3a7849de41b
sandevh <sandevhettiarachchi@gmail.com>
2025-02-25 18:57:31 +0530

    update send_email to reuse and added empty files for password reset logic

--- kuppi-server/services/auth_service.py
+++ kuppi-server/services/auth_service.py
@@ -38,8 +38,45 @@ def signup_user_service(email, name, password):
         "password": generate_password_hash(password)
     }
 
+    subject = "Your OTP for Kuppi Registration"
+
+    content = f"""
+            <div style="max-width: 600px; margin: 40px auto; padding: 25px 20px; 
+            border-radius: 12px; box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2); 
+            background-color: #000000; text-align: center; font-family: Arial, sans-serif; border: 2px solid #ffffff;" >
+
+                <h1 style="background-color: #2581eb; color: #ffffff; 
+                        padding: 20px; border-radius: 12px; 
+                        margin: 20px 0; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); 
+                        font-size: 24px; font-weight: bold;">
+                    Hello from Kuppi
+                </h1>
+
+                <p style="font-size: 20px; font-weight: 600; color: #ffffff; margin-bottom: 40px;">
+                    Use the OTP below to complete your registration.
+                </p>
+
+                <div style="display: inline-block; background-color: #f4f4f4; 
+                            padding: 15px 30px; border-radius: 12px; 
+                            box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); 
+                            font-size: 32px; font-weight: bold; color: #2c3e50; margin: 20px 0;">
+                    {otp}
+                </div>
+
+                <p style="font-size: 16px; color: #cfdadb; margin-top: 20px;">
+                    This OTP will expire in <strong>10 minutes</strong>.
+                </p>
+
+                <hr style="border: none; border-top: 1px solid #444;">
+
+                <p style="font-size: 14px; color: #cfdadb;">
+                    If you did not request this OTP, please ignore this email.
+                </p>
+            </div>
+            """
+
     # Send OTP via email
-    if not send_email(email, otp):
+    if not send_email(email, subject, content):
         del pending_users[email]  # Cleanup if email sending fails
         return {"error": "Failed to send OTP. Please try again."}, 500
 
