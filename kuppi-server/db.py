931d01b6ba9a5cf21998dd9e8a3c49d5b68ce526
sandevh <sandevhettiarachchi@gmail.com>
2025-03-03 22:04:10 +0530

    added collection names in db.py

--- kuppi-server/db.py
+++ kuppi-server/db.py
@@ -3,3 +3,8 @@ from config import Config
 
 client = MongoClient(Config.MONGO_URI)
 db = client["KuppiDB"]
+
+users_collection = db["users"]
+pending_users_collection = db["pending_users"]
+otp_storage_signup_collection = db["otp_storage_signup"]
+otp_storage_password_reset_collection = db["otp_storage_password_reset"]
