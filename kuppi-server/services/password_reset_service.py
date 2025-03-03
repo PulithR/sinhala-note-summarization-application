5454309aee16698cd4c5cfff0eed01d141924d7a
Semal Yapa <yapasemal1.dssc@gmail.com>
2025-03-03 23:49:10 +0530

    Imported the needed libraries and declared the parameters

--- kuppi-server/services/password_reset_service.py
+++ kuppi-server/services/password_reset_service.py
@@ -1,3 +1,14 @@
+import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
+from models.user_model import users_db, otp_storage_password_reset
+from services.otp_service import generate_otp
+from services.email_service import send_email
+
+MAX_OTP_ATTEMPTS = 3
+OTP_EXPIRY_SECONDS = 600  # 10 minutes
+OTP_REQUEST_COOLDOWN_SECONDS = 60 
+
 def request_password_reset_service():
   pass
 
