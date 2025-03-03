be458f84643f5e32746da4c5b1096a1bd3bdd7b6
sandevh <sandevhettiarachchi@gmail.com>
2025-03-03 19:24:41 +0530

    db test successfull!

--- kuppi-server/db.py
+++ kuppi-server/db.py
@@ -2,11 +2,4 @@ from pymongo import MongoClient
 from config import Config
 
 client = MongoClient(Config.MONGO_URI)
-db = client["KuppiCS50"]
-
-test_collection = db["test_collection"]
-
-test_user = {"name": "Test User", "email": "test@gmail.com"}
-result = test_collection.insert_one(test_user)
-
-print("Inserted into ID: ", result.inserted_id)
+db = client["KuppiDB"]
