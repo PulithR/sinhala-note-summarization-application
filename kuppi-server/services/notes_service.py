7ae40252059a7978066bb9abeadc8333d36c5927
Sandaru04 <sadmal04that@gmail.com>
2025-03-04 09:43:50 +0530

    "Implemented the get note service function by id"

--- kuppi-server/services/notes_service.py
+++ kuppi-server/services/notes_service.py
@@ -41,8 +41,21 @@ def get_notes_service():
     notes_preview = [{"_id": str(note["_id"]) if "_id" in note else None, "title": note["title"]} for note in user.get("notes", [])]
     return {"notes": notes_preview}, 200
 
-def delete_note_service(note_id):
-    pass
+def get_note_by_id_service(note_id):
+    current_user_email = get_jwt_identity()
+    user = users_collection.find_one({"email": current_user_email})
+
+    if not user:
+        return {"error": "User not found"}, 404
+
+    note = next((note for note in user.get("notes", []) if str(note.get("_id")) == note_id), None)
+
+    if not note:
+        return {"error": "Note not found"}, 404
+
+    return {"note": note}, 200
+
+
 
 def delete_all_notes_service():
     pass
