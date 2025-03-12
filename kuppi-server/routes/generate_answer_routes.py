23049fbbb77d8eead63a8d3004b8307f1c3d51a3
sandevh <sandevhettiarachchi@gmail.com>
2025-03-12 16:33:37 +0530

    configured app with gemini API key

--- kuppi-server/routes/generate_answer_routes.py
+++ kuppi-server/routes/generate_answer_routes.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required
-from services.generate_answer_services import generate_answer_service
+from services.generate_answer_service import generate_answer_service
 
 generate_answer_bp = Blueprint("generate_answer", __name__)
 
 @generate_answer_bp.route("/generate-answer", methods=["POST"])
-@jwt_required
+@jwt_required()
 def generate_answer():
     data = request.json
     response, status = generate_answer_service(data)
