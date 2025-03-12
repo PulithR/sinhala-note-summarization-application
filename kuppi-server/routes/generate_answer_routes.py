58ae1492a674d2b0d29b1b0b60581c804493a471
Semal Yapa <yapasemal1.dssc@gmail.com>
2025-03-12 15:34:24 +0530

    fixed _name_ to __name__

--- kuppi-server/routes/generate_answer_routes.py
+++ kuppi-server/routes/generate_answer_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required
 from services.generate_answer_services import generate_answer_service
 
-generate_answer_bp = Blueprint("generate_answer", _name_)
+generate_answer_bp = Blueprint("generate_answer", __name__)
 
 @generate_answer_bp.route("/generate-answer", methods=["POST"])
 @jwt_required
