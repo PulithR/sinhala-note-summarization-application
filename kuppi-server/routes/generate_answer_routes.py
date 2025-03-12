28f9aa15aa1decc7bf0fcd66bd9662a9f31160ec
Semal Yapa <yapasemal1.dssc@gmail.com>
2025-03-12 15:31:14 +0530

    Created the generate_answer_routes file

--- kuppi-server/routes/generate_answer_routes.py
+++ kuppi-server/routes/generate_answer_routes.py
@@ -1,1 +1,12 @@
-# routes will go here
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required
+from services.generate_answer_services import generate_answer_service
+
+generate_answer_bp = Blueprint("generate_answer", _name_)
+
+@generate_answer_bp.route("/generate-answer", methods=["POST"])
+@jwt_required
+def generate_answer():
+    data = request.json
+    response, status = generate_answer_service(data)
+    return jsonify(response),status
