9bb577e632dbc32c86a07be166ff97e3cbb608f9
Sandeepa <gamages570@gmail.com>
2025-03-18 23:24:19 +0530

    Added the fully implemented Logout button

--- kuppi-interface/components/LogOutButton.js
+++ kuppi-interface/components/LogOutButton.js
@@ -1,29 +1,48 @@
 import React, { useContext } from "react";
-import { AuthContext } from "../authentication/AuthContext";
 import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import { AuthContext } from "../authentication/AuthContext";
+import { LanguageContext } from "../user_preference/LanguageContext";
+import { ThemeContext } from "../user_preference/ThemeContext";
+import themeColors from "../assets/ThemeColors.json";
 
 const LogoutButton = () => {
   const { logout } = useContext(AuthContext);
+  const { language, t } = useContext(LanguageContext);
+  const { currentTheme } = useContext(ThemeContext);
 
   return (
-    <TouchableOpacity onPress={logout} style={styles.button}>
-      <Text style={styles.buttonText}>Log Out</Text>
+    <TouchableOpacity
+      style={[
+        styles.button,
+        { backgroundColor: themeColors[currentTheme].toggleBg[0] },
+      ]}
+      onPress={logout}
+    >
+      <Text
+        style={[
+          styles.buttonText,
+          { color: themeColors[currentTheme].toggleText },
+        ]}
+      >
+        {t.logout}
+      </Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
   button: {
-    backgroundColor: "#c44747",
-    padding: 10,
-    margin: 20,
-    borderRadius: 16,
+    width: "100%", // Full width from previous version
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 10,
     alignItems: "center",
     justifyContent: "center",
+    marginVertical: 10, // Using marginVertical instead of margin
   },
   buttonText: {
-    color: "#fff",
     fontSize: 16,
+    fontWeight: "bold", // Added from previous version
   },
 });
 
